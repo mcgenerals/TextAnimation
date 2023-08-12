0230261486ae8578b9273bd0c8454baa9fa1a994
mcgenerals <138998996+mcgenerals@users.noreply.github.com>
2023-08-13 01:46:47 +0900

    Update script.js
    small update for you

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function createTextAnimation(txt,elid,intvl){
+function createTextAnimation(txt,elid,intvl,noreset){
     const ani = new Animation(txt,elid);
     setInterval(ani.upd,intvl);
 }
 
 class Animation {
-    constructor(text,elementId) {
+    constructor(text,elementId,noreset) {
         this.text = text;
         this.elementId = elementId;
         this.cur = "";
+        this.noreset = noreset;
+        this.ended = false;
         this.i = 0;
         this.spl = this.text.split("");
         this.upd = this.upd.bind(this);
@@ -17,7 +19,11 @@ class Animation {
         if(this.cur==this.text){
             this.cur = "";
             this.i = 0;
+            if(this.noreset){ this.ended = true; }
             document.getElementById(this.elementId).innerHTML = "";
+            return;
+        }
+        if( this.ended ){
             return;
         }
         this.spl = this.text.split("");
