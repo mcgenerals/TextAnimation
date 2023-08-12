e95ef3a89fcd525aedb82263306df0a71fe8bff5
mcgenerals <138998996+mcgenerals@users.noreply.github.com>
2023-08-13 01:34:37 +0900

    Update script.js
    improved a lot

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-var text = "I love github!"; // Text
-var elementId = "message"; // Element's ID
-var interval = 1000; //Interval
-
-var cur = "";
-var i = 0;
-function run(){
-    if(cur==text){
-       cur = "";
-       i = 0;
-       document.getElementById(elementId).innerHTML = "";
-       return;
-    }
-    spl = text.split("");
-    cur += spl[i];
-    i++;
-    document.getElementById(elementId).innerHTML = cur;
-
+function createTextAnimation(txt,elid,intvl){
+    const ani = new Animation(txt,elid);
+    setInterval(ani.upd,intvl);
 }
-setInterval(run,interval);
-// © 2023 MCGENERALS
+
+class Animation {
+    constructor(text,elementId) {
+        this.text = text;
+        this.elementId = elementId;
+        this.cur = "";
+        this.i = 0;
+        this.spl = this.text.split("");
+        this.upd = this.upd.bind(this);
+    }
+
+    upd(){
+        if(this.cur==this.text){
+            this.cur = "";
+            this.i = 0;
+            document.getElementById(this.elementId).innerHTML = "";
+            return;
+        }
+        this.spl = this.text.split("");
+        this.cur += this.spl[this.i];
+        this.i++;
+        document.getElementById(this.elementId).innerHTML = this.cur;
+    }
+    // © 2023 MCGENERALS
+}
